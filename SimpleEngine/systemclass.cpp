98475fc32f6f1f4e513c0cdc0c784a35856827df
Piotr Milewski <milewski.piotr.m@gmail.com>
2018-02-28 00:44:15 +0100

    Building Framework

---
+++ SimpleEngine/systemclass.cpp
@@ -0,0 +1,248 @@
+///////////////////////////////
+// Filename: systemclass.cpp   //
+///////////////////////////////
+#include "systemclass.h"
+
+SystemClass::SystemClass()
+{
+	m_Input = nullptr;
+	m_Graphics = nullptr;
+}
+
+SystemClass::SystemClass(const SystemClass &)
+{
+}
+
+SystemClass::~SystemClass()
+{
+}
+
+bool SystemClass::Initialize()
+{
+	int screenWidth{ 0 }, screenHeight{ 0 };
+	bool result;
+
+	InitializeWindows(screenWidth, screenHeight);
+
+	m_Input = new InputClass;
+	if (m_Input == nullptr)
+	{
+		return false;
+	}
+	
+	m_Input->Initialize();
+
+	m_Graphics = new GraphicsClass;
+	if (m_Graphics == nullptr)
+	{
+		return false;
+	}
+
+	result = m_Graphics->Initialize(screenWidth, screenHeight, m_hwnd);
+	if (!result)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void SystemClass::Shutdown()
+{
+	if (m_Graphics)
+	{
+		m_Graphics->Shutdown();
+		delete m_Graphics;
+		m_Graphics = nullptr;
+	}
+
+	if (m_Input)
+	{
+		delete m_Input;
+		m_Input = nullptr;
+	}
+
+	ShutdownWindows();
+
+	return;
+}
+
+void SystemClass::Run()
+{
+	MSG msg;
+	bool done, result;
+
+	ZeroMemory(&msg, sizeof(MSG));
+
+	done = false;
+	while (!done)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		if (msg.message == WM_QUIT)
+		{
+			done = true;
+		}
+		else
+		{
+			result = Frame();
+			if (!result)
+			{
+				done = true;
+			}
+		}
+	}
+
+	return;
+}
+
+LRESULT SystemClass::MessageHandler(HWND hwnd, UINT umsg, WPARAM wparam, LPARAM lparam)
+{
+	switch (umsg)
+	{
+		case WM_KEYDOWN:
+		{
+			// If a key is pressed send it to the input object so it can record that state.
+			m_Input->KeyDown(static_cast<unsigned int>(wparam));
+			return 0;
+		}
+		case WM_KEYUP:
+		{
+			// If a key is released then send it to the input object so it can unset the state for that key.
+			m_Input->KeyUp(static_cast<unsigned int>(wparam));
+			return 0;
+		}
+		// Any other messages send to the default message handler as our application won't make use of them.
+		default:
+			return DefWindowProc(hwnd, umsg, wparam, lparam);
+	}
+}
+
+bool SystemClass::Frame()
+{
+	bool result;
+
+	if (m_Input->IsKeyDown(VK_ESCAPE))
+	{
+		return false;
+	}
+
+	result = m_Graphics->Frame();
+	if (!result)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void SystemClass::InitializeWindows(int &screenWidth, int &screenHeight)
+{
+	WNDCLASSEX wc;
+	DEVMODE dmScreenSettings;
+	int posX{ 0 }, posY{ 0 };
+
+	ApplicationHandle = this;
+
+	m_hinstance = GetModuleHandle(NULL);
+	
+	m_applicationName = L"Engine";
+
+	// Setup the windows class with default settings.
+	wc.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = m_hinstance;
+	wc.hIcon = LoadIcon(NULL, IDI_WINLOGO);
+	wc.hIconSm = wc.hIcon;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = m_applicationName;
+	wc.cbSize = sizeof(WNDCLASSEX);
+
+	// Reguster the window class
+	RegisterClassEx(&wc);
+
+	// Check for resolution of the clients desktop screen.
+	screenWidth = GetSystemMetrics(SM_CXSCREEN);
+	screenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+	if (FULL_SCREEN)
+	{
+		memset(&dmScreenSettings, 0, sizeof(dmScreenSettings));
+		dmScreenSettings.dmSize = sizeof(dmScreenSettings);
+		dmScreenSettings.dmPelsWidth = static_cast<unsigned long>(screenWidth);
+		dmScreenSettings.dmPelsHeight = static_cast<unsigned long>(screenHeight);
+		dmScreenSettings.dmBitsPerPel = 32;
+		dmScreenSettings.dmFields = DM_BITSPERPEL | DM_PELSHEIGHT | DM_PELSWIDTH;
+
+		// Change display settings to fullscreen
+		ChangeDisplaySettings(&dmScreenSettings, CDS_FULLSCREEN);
+
+		posX = posY = 0; //top left corner
+
+	}
+	else
+	{
+		screenWidth = 800;
+		screenHeight = 600;
+
+		posX = (GetSystemMetrics(SM_CXSCREEN) - screenWidth) / 2;
+		posY = (GetSystemMetrics(SM_CYSCREEN) - screenHeight) / 2;
+	}
+
+	m_hwnd = CreateWindowEx(WS_EX_APPWINDOW, m_applicationName, m_applicationName,
+		WS_CLIPSIBLINGS | WS_CLIPCHILDREN | WS_POPUP, posX, posY, screenWidth,
+		screenHeight, NULL, NULL, m_hinstance, NULL);
+
+	ShowWindow(m_hwnd, SW_SHOW);
+	SetForegroundWindow(m_hwnd);
+	SetFocus(m_hwnd);
+
+	ShowCursor(false);
+
+	return;
+}
+
+void SystemClass::ShutdownWindows()
+{
+	ShowCursor(true);
+
+	if (FULL_SCREEN)
+	{
+		ChangeDisplaySettings(NULL, 0);
+	}
+
+	DestroyWindow(m_hwnd);
+	m_hwnd = NULL;
+
+	ApplicationHandle = NULL;
+
+	return;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT umsg, WPARAM wparam, LPARAM lparam)
+{
+	switch (umsg)
+	{	
+		case WM_DESTROY:
+		{
+			PostQuitMessage(0);
+			return 0;
+		}
+		case WM_CLOSE:
+		{
+			PostQuitMessage(0);
+			return 0;
+		}
+
+		default:
+			return ApplicationHandle->MessageHandler(hwnd, umsg, wparam, lparam);
+	}
+}
